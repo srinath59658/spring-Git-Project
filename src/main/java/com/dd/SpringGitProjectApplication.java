6f9d3536c18f249a04120e30be89fcb257b62d35
srinath59658 <srinath59658@gmail.com>
2025-07-27 16:28:44 +0530

    Initial commit

---
+++ src/main/java/com/dd/SpringGitProjectApplication.java
@@ -0,0 +1,14 @@
+package com.dd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitProjectApplication.class, args);
+		System.out.println("1-java");
+	}
+
+}
