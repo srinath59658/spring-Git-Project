82f71e7d362d9a54e6043febdbed4b1d5f5aa250
srinath59658 <srinath59658@gmail.com>
2025-07-27 16:49:19 +0530

    Updated login flow in dev branch

--- src/main/java/com/dd/SpringGitProjectApplication.java
+++ src/main/java/com/dd/SpringGitProjectApplication.java
@@ -9,6 +9,7 @@ public class SpringGitProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGitProjectApplication.class, args);
 		System.out.println("1-java");
+		System.out.println("2-java");
 	}
 
 }
